3f4851f2ba3d4c973e4c52ecf17561846c7be008
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-14 10:52:44 +0200

    ss

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -1,4 +1,4 @@
-$("footer").click(function(){ //en funktion som lyssnar efter ett tryck på #medier
+$("#medier").click(function(){ //en funktion som lyssnar efter ett tryck på #medier
 
   $('html, body').animate({
         scrollTop: $("footer").offset().top-60
