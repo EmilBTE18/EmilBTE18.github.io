28b9f929a0cdc1dfc79896e248b2161ce55e487c
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-18 10:49:16 +0200

    följ oss

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -1,7 +1,8 @@
-$("#about").click(function(){ //en funktion som lyssnar efter ett tryck på #medier
+$("#medier").click(function(){ //en funktion som lyssnar efter ett tryck på #medier
+console.log("test");
 
   $('html, body').animate({
-        scrollTop: $("#Omoss").offset().top-60
+        scrollTop: $("#Media").offset()
     }, 2500);
 
 });
