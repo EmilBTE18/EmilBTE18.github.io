1239af8b6bdf6fc77bdd2a4935813367509848f3
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-18 10:30:57 +0200

    sdds

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -1,15 +1,7 @@
-$("#medier").click(function(){ //en funktion som lyssnar efter ett tryck på #medier
+$("#about").click(function(){ //en funktion som lyssnar efter ett tryck på #medier
 
   $('html, body').animate({
-        scrollTop: $("footer").offset().top-60
+        scrollTop: $("#Omoss").offset().top-60
     }, 2500);
 
-
-
 });
-
-function scroll_to('footer'){
-   if (div.scrollTop < div.scrollHeight - div.clientHeight)
-        div.scrollTop += 10; // move down
-
-}
