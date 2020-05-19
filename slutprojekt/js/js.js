0b97d8e352436a144a491e309d38a5019474704b
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-19 19:24:47 +0200

    topnav halvkalr

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -2,7 +2,16 @@ $("#medier").click(function(){ //en funktion som lyssnar efter ett tryck på #me
 console.log("test");
 
   $('html, body').animate({
-        scrollTop: $("#Media").offset().top-60
+        scrollTop: $("#Media").offset().top-60 //åker ned på sidan till #Media
     }, 5000);
 
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
