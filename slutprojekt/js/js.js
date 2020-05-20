621d69f9ac6bc023369c55a3b4c273e9497266fb
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-20 15:58:02 +0200

    topnav hamburger

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -6,3 +6,12 @@ console.log("test");
     }, 5000);
 
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
