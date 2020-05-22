1aafacd606d6c00c7dea3c7133b0e68fbe477ac5
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-23 00:50:59 +0200

    kommenterad javascript och diskreta domskript lite små fix kvar

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -5,10 +5,11 @@ $("#medier").click(function () { //en funktion som lyssnar efter ett tryck på #
 });
 
 function myFunction() {
-    var x = document.getElementById("länkar");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var x = document.getElementById("länkar"); // länkarna i dropdown
+    if (x.style.display === "block") { //om man trycker på hamburger menyn = stängd dropdown
+        x.style.display = "none";  //om man trycker på hamburger menyn = öppen dropdown
     } else {
-        x.style.display = "block";
+        x.style.display = "block"; // stängd dropdown när man inte tryckt på hamburger menyn
     }
 }
+$("länkar").css("display","none"); // sätter display none på länkarna så dem syns när javascript är av
