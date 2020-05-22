a568f1ecde6282a76830bca0e887dd5470558c21
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-22 15:55:59 +0200

    top nav

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -4,29 +4,14 @@ $("#medier").click(function(){ //en funktion som lyssnar efter ett tryck på #me
   $('html, body').animate({
     scrollTop: $("#Media").offset().top-60
   }, 5000);
-  
-});
-
-$(".dropknapp").show();
-$("#minDropdown").addClass("dropdown-innehåll");
-/* När användaren klickar på knappen,
-växla mellan att dölja och visa innehållet */
-$(".dropknapp").click(function(){
-  document.getElementById("minDropdown").classList.toggle("show");
-
 
 });
-// Den stänger dropdown
-window.onclick = function(event) {
-  if (!event.target.matches('.dropknapp')) {
-    var dropdowns = document.getElementsByClassName("dropdown-innehåll");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+
+function myFunction() {
+  var x = document.getElementById("länkar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-
 }
