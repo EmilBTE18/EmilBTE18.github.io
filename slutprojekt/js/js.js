9e1a58ac0a96d6a577b1d6d42772743ab3cd69ef
EmilBTE18 <55192282+EmilBTE18@users.noreply.github.com>
2020-05-23 12:32:33 +0200

    validerad kod, och allting annat klart = JAG ÄR FCKING KLAR!!!

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -1,15 +1,17 @@
+
 $("#medier").click(function () { //en funktion som lyssnar efter ett tryck på #medier
     $("html, body").animate({
         scrollTop: $("#Media").offset().top - 60
-    }, 5000);
+    }, 4000);
 });
 
 function myFunction() {
     var x = document.getElementById("länkar"); // länkarna i dropdown
-    if (x.style.display === "block") { //om man trycker på hamburger menyn = stängd dropdown
-        x.style.display = "none";  //om man trycker på hamburger menyn = öppen dropdown
+    if (x.style.display === "none") { //om man trycker på hamburger menyn = öppen dropdown
+        x.style.display = "block";  //om man trycker på hamburger menyn = stängd dropdown
     } else {
-        x.style.display = "block"; // stängd dropdown när man inte tryckt på hamburger menyn
+        x.style.display = "none"; // stängd dropdown när man inte tryckt på hamburger menyn
     }
 }
-$("länkar").css("display","none"); // sätter display none på länkarna så dem syns när javascript är av
+
+$("#länkar").hide(); // gömmer länkarna
